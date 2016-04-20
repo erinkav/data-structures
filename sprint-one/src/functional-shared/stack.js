faede3a1cdb305ad45199e214b78307a47a27246
Erin Kavanaugh <elkavanaugh@gmail.com>
2016-04-20 13:08:11 -0700

    functional and functional-shared passing for Stack

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,26 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newInstance = {}; 
+  newInstance.length = 0; 
+  _.extend(newInstance, stackMethods); 
+  return newInstance; 
 };
 
-var stackMethods = {};
+var stackMethods = {
+	pop : function() {
+		var popped = this[this.length-1]; 
+		delete this[this.length-1];
+		this.length--;
+		return popped;
+	}, 
+	push : function(value) {
+		this[this.length] = value; 
+		this.length++; 
+	},
+	size : function() {
+		return this.length > 0 ? this.length: 0; 
+	}
+};
 
 
