3f37ec75eb0ee7be8e2eb46a5a6aa0a7ab4a39cf
erinkav <elkavanaugh+hr@gmail.com>
2016-04-20 21:16:56 -0700

    Passing all tests

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,36 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.test = 'passing';
 };
 
 
+Stack.prototype.push = function(value) {
+  this[this.size()] = value;
+};
+
+Stack.prototype.pop = function() {
+  var popped = this[this.size() - 1];
+  delete this[this.size() - 1];
+  return popped;
+};
+
+Stack.prototype.size = function() {
+  var length = 0;
+  while (this[length] !== undefined) {
+    length++;
+  }
+  return length; 
+};
+
+var newInstance = new Stack(); 
+
+
+// Do:
+// Work within the src/pseudoclassical/ folder
+// Capitalize your function name to indicate to others that it is intended to be run with the keyword new
+// Use the keyword new when instantiating your class
+// Use the keyword this in your constructor
+// Don't:
+// Declare the instance explicitly
+// Return the instance explicitly
